2c2b505272612460c5ba0184257bd3000e0fdd2b
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-11-20 12:32:01 +0100

    tailwind

---
+++ quiz-app-frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14122b",
+      }
+    },
+  },
+  plugins: [],
+};
+
