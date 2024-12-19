62ea0eccc1a2b3f3a21971aaa068900fea230109
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-12-19 12:34:44 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
 
 export default defineConfig(({ command, mode }) => {
-   const serverConfig =
-       command === 'serve'
-          ? {
-             host: 'frontend.quiz-app-frontend.test',
-             port: 5173,
-             https: {
-               key: fs.readFileSync('./cert/key.pem'),
-               cert: fs.readFileSync('./cert/cert.pem'),
-             },
-             proxy: {
-               '/api': {
-                 target: 'https://localhost:3000/', // Backend server
-                 changeOrigin: true,
-                 secure: false,
-                 rewrite: (path) => path.replace(/^\/api/, ''),
-               },
-             },
-           }
-            : undefined;
 
   return {
     plugins: [react()],
-    server: serverConfig,
   }
 })
