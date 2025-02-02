c76cb889797ffd83bdcdae13dd2d52687259bd1a
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-02-02 16:35:45 +0100

    logo

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -13,7 +13,7 @@ const AppLayout: React.FC<AppLayoutProps> = () => {
             <div className={'flex flex-col w-full'}>
                 <div className={'flex h-24 bg-gray-700 p-4 items-center'}>
                     <Link to={'/'} className={'flex flex-row items-center' }>
-                        <img src={'public/iu-quiz-app-logo.png'} alt={'logo'} className={'h-16 w-16'} />
+                        <img src={`${import.meta.env.BASE_URL}iu-quiz-app-logo.png`} alt={'logo'} className={'h-16 w-16'} />
                         <span className={'text-white'}>
                             uiz-App
                         </span>
