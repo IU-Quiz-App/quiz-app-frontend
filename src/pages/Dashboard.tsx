d422c6fd288489ca024d3b48f489344c2bf159e5
jannawitt <jannawitt@gmx.de>
2025-02-12 20:01:53 +0100

    Add Headerbar, Welcometext Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,23 +1,29 @@
 import Button from "../components/Button.tsx";
 
 const Dashboard: React.FC = () => {
-
     return (
-        <div className={'w-full flex items-center justify-center h-full gap-6'}>
-            <Button variant={'secondary'} className={'w-fit h-fit'} route={'/quiz/question'}>
-                Spiel starten
-            </Button>
+        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
+            {/* Welcome */}
+            <h1 className="text-2xl font-bold text-blue-700 text-center">
+                Willkommen bei der IU-Quiz-App!
+            </h1>
 
-            <Button variant={'primary'} className={'w-fit h-fit'} route={'/question/form'}>
-                Frage erstellen
-            </Button>
+            {/* Buttons */}
+            <div className="flex items-center justify-center gap-6">
+                <Button variant={'secondary'} className={'w-fit h-fit'} route={'/quiz/question'}>
+                    Spiel starten
+                </Button>
 
-            <Button variant={'primary'} className={'w-fit h-fit'} route={'/questions'}>
-                Alle Fragen anzeigen
-            </Button>
+                <Button variant={'primary'} className={'w-fit h-fit'} route={'/question/form'}>
+                    Frage erstellen
+                </Button>
+
+                <Button variant={'primary'} className={'w-fit h-fit'} route={'/questions'}>
+                    Alle Fragen anzeigen
+                </Button>
+            </div>
         </div>
-    )
-
+    );
 }
 
 export default Dashboard;
