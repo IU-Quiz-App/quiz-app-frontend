071189cc979791b585c7b1ae03ddb635b962cbb6
DevOpsPH <philipp-hoyer@web.de>
2025-02-13 14:11:45 +0100

    add question loading

--- src/pages/question/QuestionTableWrapper.tsx
+++ src/pages/question/QuestionTableWrapper.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect, useState } from 'react';
-import {getAllQuestions} from "@services/Api.ts";
-import {Question} from "@services/Types.ts";
+import { getAllQuestions } from "@services/Api.ts";
+import { Question } from "@services/Types.ts";
 import Box from "@components/Box.tsx";
 import QuestionTable from "@pages/question/QuestionTable.tsx";
 import Button from "@components/Button.tsx";
@@ -8,34 +8,42 @@ import Button from "@components/Button.tsx";
 export const FlyersTableWrapper: FC = () => {
 
     const [questions, setQuestions] = useState<Question[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchData = async () => {
-            const questions = await getAllQuestions();
-            setQuestions(questions);
-        }
-
-        fetchData()
-            .catch((error) => {
+            try {
+                const questions = await getAllQuestions();
+                setQuestions(questions);
+            } catch (error) {
                 console.error(error);
-            });
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
 
+    if (loading) {
+        return <Box>Loading...</Box>;
+    }
 
-  return (
-      <Box className={'w-full flex-col gap-4'}>
-          <div className={'flex flex-row justify-between w-full'}>
-              <Button variant={'primary'} className={'w-fit h-fit'} route={'/dashboard'}>
-                  Zurück
-              </Button>
 
-              <Button variant={'primary'} className={'w-fit h-fit whitespace-nowrap'} route={'/question/form'}>
-                  Frage hinzufügen
-              </Button>
-          </div>
-         <QuestionTable questions={questions} />
-      </Box>
-  );
+    return (
+        <Box className={'w-full flex-col gap-4'}>
+            <div className={'flex flex-row justify-between w-full'}>
+                <Button variant={'primary'} className={'w-fit h-fit'} route={'/dashboard'}>
+                    Zurück
+                </Button>
+
+                <Button variant={'primary'} className={'w-fit h-fit whitespace-nowrap'} route={'/question/form'}>
+                    Frage hinzufügen
+                </Button>
+            </div>
+            <QuestionTable questions={questions} />
+        </Box>
+    );
 };
 
 export default FlyersTableWrapper;
