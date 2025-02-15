2704a64f436b43c306334a75fea369115fd83753
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-02-15 14:50:33 +0100

    fixed api endpoint

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,4 +1,4 @@
 export default {
-    ApiURL: import.meta.env.MODE === "development" ? "/api" : (import.meta.env.VITE_API_URL ?? "https://api.example.de"),
+    ApiURL: import.meta.env.MODE === "development" ? "/api" : (import.meta.env.VITE_API_URL ?? "https://api.dev.iu-quiz.de"),
     BaseUrl: import.meta.env.BASE_URL,
 };
