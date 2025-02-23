5189b58955526ea313eb88502b2455d435235631
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-02-23 14:28:24 +0100

    websocket test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,21 @@
 import Button from "../components/Button.tsx";
+import useWebSocket from "react-use-websocket";
+import {useEffect} from "react";
 
 const Dashboard: React.FC = () => {
+
+    const WEBSOCKET_URL = "ws.dev.iu-quiz.de";
+
+    const { sendMessage, lastMessage } = useWebSocket(WEBSOCKET_URL, {
+        shouldReconnect: () => true,
+        reconnectAttempts: 10,
+        reconnectInterval: 3000,
+    });
+
+    useEffect(() => {
+        sendMessage('Hello from the IU-Quiz-App!', true);
+    }, [lastMessage]);
+
     return (
         <div className="w-full h-full flex flex-col items-center justify-center gap-6">
             {/* Welcome */}
@@ -20,6 +35,10 @@ const Dashboard: React.FC = () => {
 
                 <Button variant={'primary'} className={'w-fit h-fit'} route={'/questions'}>
                     Alle Fragen anzeigen
+                </Button>
+
+                <Button variant={'tertiary'} className={'w-fit h-fit'} onClick={() => sendMessage('Hello from the IU-Quiz-App!', true)}>
+                    WebSocket Test
                 </Button>
             </div>
         </div>
