c938dd3b0a14e5083ee6fe4418ed47673f8c8d81
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-04-02 22:30:17 +0200

    github action

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,6 +1,6 @@
 export default {
     ApiURL: import.meta.env.VITE_API_URL,
-    WebsocketURL: import.meta.env.VITE_WS_URL ?? "wss://ws.dev.iu-quiz.de",
+    WebsocketURL: import.meta.env.VITE_WS_URL,
     MSALClientId: import.meta.env.VITE_MSAL_CLIENT_ID,
     MSALTenantId: import.meta.env.VITE_MSAL_TENANT_ID,
     MSALRedirectUri: import.meta.env.VITE_MSAL_REDIRECT_URI,
